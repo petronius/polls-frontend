2c87adbc2781626aca34146b6175d3d2521339b0
Michael Schuller <msc@artbutler.com>
2017-07-17 17:44:28 +0200

    Load the questions list
    Added loading of the list of questions on component initialization,
    as well as a seperate QuestionInfo component for displaying them.

--- src/QuestionsList.js
+++ src/QuestionsList.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+import API_MAP from './APIMap';
+
 export default class QuestionsList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: []
+    }
+  }
+
+  componentDidMount() {
+    var me = this;
+    this.serverRequest = axios.get(API_MAP.questions_list)
+      .then(function(result) {
+        me.setState({
+          questions: result.data
+        });
+      });
+  }
+
   render() {
     return (
       <div className="QuestionsList">
-        List of questions
+        <h1>Questions</h1>
+        <div className="QuestionsList-inner">
+          {this.state.questions.map(function(question, i) {
+            return <QuestionInfo key={i} question={question} />;
+          })}
+        </div>
       </div>
     );
+  }
+}
+
+
+class QuestionInfo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionDetails: props.question
+    };
+  }
+  
+  render() {
+    return (
+      <div className="QuestionInfo">
+        {this.state.questionDetails.question}
+      </div>
+    )
   }
 }
