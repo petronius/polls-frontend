6f909766ade30a8abef53f0e6e8fd23ae12f0a13
Michael Schuller <msc@artbutler.com>
2017-07-17 17:57:00 +0200

    Add a quick note about the lack of pagination

--- src/QuestionsList.js
+++ src/QuestionsList.js
@@ -9,13 +9,27 @@ export default class QuestionsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      questions: []
+      questions: [],
     }
   }
 
   componentDidMount() {
     var me = this;
-    this.serverRequest = axios.get(API_MAP.questions_list)
+    this.serverRequest = axios.get(API_MAP.questions_list, {
+        // In principle, this API supports more than one page, as described in the API:
+        // 
+        //   http://docs.pollsapi.apiary.io/#reference/question/questions-collection/list-all-questions
+        //
+        // But in practice, there is a) only one page of questions, and b) the
+        // API doesn't expose the correct headers via Access-Control-Expose-Headers
+        // that we need in order to read the pagination info in the "Link"
+        // response header.
+        //
+        // More info: https://stackoverflow.com/a/37931084/1583183
+        params: {
+          page: 1
+        }
+      })
       .then(function(result) {
         me.setState({
           questions: result.data
