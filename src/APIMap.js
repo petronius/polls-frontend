10d2e5d6086f12af372edd228582a17f5edfb4fe
Michael Schuller <msc@artbutler.com>
2017-07-17 17:38:16 +0200

    Add a list of endpoints for polls
    A simple config file for storing the map of endpoints we'll be using

---
+++ src/APIMap.js
@@ -0,0 +1,10 @@
+
+const API_SERVER = "https://polls.apiblueprint.org";
+
+const endpoints = {
+    "root": `${API_SERVER}/`,
+    "questions_list": `${API_SERVER}/questions`,
+    "question_details": `${API_SERVER}/questions/:id`
+};
+
+export default endpoints;
